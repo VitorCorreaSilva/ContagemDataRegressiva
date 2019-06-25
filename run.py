a38ae87d0e785441d104df0cc22ad8e30ab6449b
Vitor Correa <vitor.correa@gruporbs.com.br>
2019-06-25 16:01:49 -0300

    cmt14

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -13,5 +14,6 @@ def about():
     return render_template("layout.html")
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
